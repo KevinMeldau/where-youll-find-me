950ebfc5bee772167e4da3d3deb357de9ae52597
Kevin Meldau <me@kevinmeldau.com>
2025-10-23 23:08:11 -0400

    Text edits

--- src/pages/P1Intro.tsx
+++ src/pages/P1Intro.tsx
@@ -11,12 +11,10 @@ const P1Intro = () => {
          {/* Scrollable text area */}
          <div className="overflow-y-auto flex-1 mt-2 px-4">
             <h1 className="text-xl font-bold mb-2">
-               From the start, this was more than a trip. It’s a story I’ve been writing for you.
-            </h1>
-            <p className="mb-4">
                You’re about to do something bold. A little wild.<br />A little scary. And, if you let it,
                something unforgettable. I need you to trust me.
-            </p>
+            </h1>
+            <br />
             <p className="mb-4">
                <span className="font-bold">This is the biggest love letter I’ve ever written you</span>. And it isn’t written in ink. It’s
                written in the details only you would notice.
@@ -25,7 +23,7 @@ const P1Intro = () => {
                <span className="font-bold">You’re not going to work on Monday</span> , I’ve spoken to Hannah about this (listen to her voice recording below).
                <br /><br />
                <span className="font-bold">I will not reply to your
-               messages, not yet anyway.</span> But I’m here. In every word. This is real. It’s happening. And it’s all for
+               messages, not yet anyway.</span> This is real. It’s happening. And it’s all for
                you.
             </p>
          </div>
